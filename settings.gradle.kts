b149b30c7d95d28113489f0d0628c4f804aa54d2
st10439842 <160724395+st10439842@users.noreply.github.com>
2024-05-06 13:59:21 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cat app"
+include(":app")
+ 
